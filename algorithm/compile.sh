aed7085dae7b2aa8ed5a23996a5f7512ac84bd86
Paul Lammertsma <paul@lammertsma.dev>
2023-06-04 19:14:22 +0200

    adjusted TS imports rewrite for Linux

--- algorithm/compile.sh
+++ algorithm/compile.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 npx tsc
-find generated/ -type f -name '*.js' -print0 | xargs -0 sed -i '' -E 's/from "([^"]+)";$/from "\1.js";/g'
+
+if [ "$(uname)" == "Darwin" ]; then
+	# Mac:
+	find generated/ -type f -name '*.js' -print0 | xargs -0 sed -i '' -E 's/from "([^"]+)";$/from "\1.js";/g'
+elif [ "$(uname)" == "Linux" ]; then
+	# Linux:
+	find generated/ -type f -name '*.js' -print0 | xargs -0 -I {} sed -i '{}' -e 's/from "\([^"]*\)";/from "\1.js";/g'
+else
+	echo "Sorry, your OS type is not supported"
+fi
