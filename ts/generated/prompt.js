2c2a4ef9cc046a93b40092e6a796c24bd0239a48
Paul Lammertsma <paul@lammertsma.dev>
2023-06-06 03:56:23 +0200

    bootstrap UI passes along inputs from form and outputs to the top of the page

--- ts/generated/prompt.js
+++ ts/generated/prompt.js
@@ -1,32 +1,44 @@
-export function Prompt(ask) {
-    const result = (window.prompt(ask) || "").replace(/\s/g, "");
+export function Prompt(id, ask) {
+    const input = document.querySelector("#" + id);
+    const result = input ? input.value : (window.prompt(ask) || "").replace(/\s/g, "");
     if (!result.length) {
-        Fail("Not a valid string");
+        Fail(id, "Not a valid string");
     }
     return result;
 }
-export function PromptInt(ask, min = Number.MIN_SAFE_INTEGER, max = Number.MAX_SAFE_INTEGER, set = undefined) {
-    const result = parseInt(window.prompt(ask) || "");
+export function PromptInt(id, ask, min = Number.MIN_SAFE_INTEGER, max = Number.MAX_SAFE_INTEGER, set = undefined) {
+    const input = document.querySelector("#" + id);
+    const result = input ? parseInt(input.value) : parseInt(window.prompt(ask) || "");
     if (isNaN(result)) {
-        Fail("Not a valid number");
+        Fail(id, "Not a valid number");
     }
     if (result < min || result > max) {
-        Fail("Number " + result + " must be >= " + min + " and <= " + max);
+        Fail(id, "Number " + result + " must be between " + min + " and " + max);
     }
     if (set && !set.has(result)) {
-        Fail("Number " + result + " must be one of " + Array.from(set).join(", "));
+        Fail(id, "Number " + result + " must be one of " + Array.from(set).join(", "));
     }
     return result;
 }
-export function Fail(output) {
+export function Fail(id, output) {
     console.error(output);
-    Output(output);
+    const input = document.querySelector("#" + id);
+    if (input) {
+        input.classList.add("is-invalid");
+        const feedback = input.parentElement?.getElementsByClassName("invalid-feedback");
+        if (feedback?.length) {
+            feedback[0].innerText = String(output);
+        }
+    }
+    else {
+        Output(output);
+    }
     throw new Error("Stopped execution.");
 }
 export function Output(output) {
     const out = typeof output === "string" ? output : JSON.stringify(output, undefined, 2);
-    document.getElementById("output").innerText = out;
-    document.getElementById("outputBox").classList.remove("d-none");
-    document.getElementById("outputBox").scrollIntoView({ behavior: "smooth", inline: "nearest" });
+    document.querySelector("#output").innerText = out;
+    document.querySelector("#outputBox").classList.remove("d-none");
+    document.querySelector("#outputBox").scrollIntoView({ behavior: "smooth", inline: "nearest" });
 }
 //# sourceMappingURL=prompt.js.map
--- ts/prompt.ts
+++ ts/prompt.ts
@@ -1,38 +1,52 @@
-export function Prompt(ask: string): string {
-    const result = (window.prompt(ask) || "").replace(/\s/g, "");
+export function Prompt(
+    id: string, 
+    ask: string): string {
+    const input = document.querySelector<HTMLInputElement>("#" + id);
+    const result = input ? input.value : (window.prompt(ask) || "").replace(/\s/g, "");
     if (!result.length) {
-        Fail("Not a valid string");
+        Fail(id, "Not a valid string");
     }
     return result;
 }
 
 export function PromptInt(
+    id: string,
     ask: string,
     min: number = Number.MIN_SAFE_INTEGER,
     max: number = Number.MAX_SAFE_INTEGER,
     set: Set<number>|undefined = undefined): number {
-        const result = parseInt(window.prompt(ask) || "");
+        const input = document.querySelector<HTMLInputElement>("#" + id);
+        const result = input ? parseInt(input.value) : parseInt(window.prompt(ask) || "");
         if (isNaN(result)) {
-            Fail("Not a valid number");
+            Fail(id, "Not a valid number");
         }
         if (result < min || result > max) {
-            Fail("Number " + result + " must be >= " + min + " and <= " + max);
+            Fail(id, "Number " + result + " must be between " + min + " and " + max);
         }
         if (set && !set.has(result)) {
-            Fail("Number " + result + " must be one of " + Array.from(set).join(", "));
+            Fail(id, "Number " + result + " must be one of " + Array.from(set).join(", "));
         }
         return result
 }
 
-export function Fail<T>(output: T) {
+export function Fail<T>(id: string, output: T) {
     console.error(output);
-    Output(output);
+    const input = document.querySelector<HTMLInputElement>("#" + id);
+    if (input) {
+        input.classList.add("is-invalid");
+        const feedback = input.parentElement?.getElementsByClassName("invalid-feedback");
+        if (feedback?.length) {
+            (<HTMLElement>feedback[0]).innerText = String(output);
+        }
+    } else {
+        Output(output);
+    }
     throw new Error("Stopped execution.");
 }
 
 export function Output<T>(output: T) {
     const out = typeof output === "string" ? output : JSON.stringify(output, undefined, 2);
-    document.getElementById("output")!.innerText = out;
-    document.getElementById("outputBox")!.classList.remove("d-none");
-    document.getElementById("outputBox")!.scrollIntoView({ behavior: "smooth", inline: "nearest" });
+    document.querySelector<HTMLInputElement>("#output")!.innerText = out;
+    document.querySelector("#outputBox")!.classList.remove("d-none");
+    document.querySelector("#outputBox")!.scrollIntoView({ behavior: "smooth", inline: "nearest" });
 }
