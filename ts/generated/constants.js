1084d54f40efe5408e67b988e3d1b9212b8f1c82
Valentin M <vmercier@me.com>
2023-07-03 17:38:06 +0200

    Algorithm timeout fixes for combinations of 5-7 players

--- ts/generated/constants.js
+++ ts/generated/constants.js
@@ -4,14 +4,29 @@ export const NUM_PLAYERS_DOUBLE = 4;
 export const MAX_COURTS = 4;
 export const MAX_PLAYERS = 12;
 export const MAX_SIMULATION_TIME_SECONDS = 30;
-export const MAX_CONSTRAINT_RELAXINGS = 4;
+export const MAX_CONSTRAINT_RELAXINGS = 5;
 export const MAX_ATTEMPTS_TO_MAKE_ROTATION_PROPOSALS = 15000;
 export const MAX_ROTATION_PROPOSALS_PER_NODE = 15000;
 export const MAX_SUBOPTIMAL_ROTATION_PROPOSAL_ALTERNATIVES_PER_NODE = 1;
 export function ConstraintsForRelaxingsCount(relaxings_count, stage) {
-    let double_with_spread = 1;
-    let double_against_spread = 1;
-    let double_against_cooldown = stage.n_players > 4 ? 2 : 0;
+    // Invariant parameters.
+    const preferred_max_spread = 1;
+    const preferred_double_against_min_cooldown = 2;
+    const resting_probability = stage.n_resting_players_per_rotation / stage.n_players;
+    const single_probability = stage.n_single_players / stage.n_players;
+    const double_probability = stage.n_double_players / stage.n_players;
+    const single_against_probability = Math.pow(single_probability, 2) / NChooseK(stage.n_single_players - 1, 1);
+    const double_with_probability = Math.pow(double_probability, 2) / NChooseK(stage.n_double_players - 1, 1);
+    // Relaxable parameters.
+    let single_cooldown = CooldownForProbability(single_probability);
+    let double_cooldown = CooldownForProbability(double_probability);
+    let double_with_spread = preferred_max_spread;
+    let double_against_spread = preferred_max_spread;
+    let double_against_cooldown = stage.n_players > 4 ? preferred_double_against_min_cooldown : 0;
+    if (relaxings_count-- > 0) {
+        single_cooldown = 0;
+        double_against_cooldown = 0;
+    }
     if (relaxings_count-- > 0) {
         ++double_against_spread;
     }
@@ -27,24 +42,20 @@ export function ConstraintsForRelaxingsCount(relaxings_count, stage) {
     if (relaxings_count > 0) {
         throw new Error("Unimplemented relaxing logic");
     }
+    // Output.
     const max_spreads = {
-        [HappeningType.RESTING]: 1,
-        [HappeningType.PLAYING_SINGLE]: 1,
-        [HappeningType.PLAYING_SINGLE_AGAINST]: 1,
-        [HappeningType.PLAYING_DOUBLE]: 1,
+        [HappeningType.RESTING]: preferred_max_spread,
+        [HappeningType.PLAYING_SINGLE]: preferred_max_spread,
+        [HappeningType.PLAYING_SINGLE_AGAINST]: preferred_max_spread,
+        [HappeningType.PLAYING_DOUBLE]: preferred_max_spread,
         [HappeningType.PLAYING_DOUBLE_AGAINST]: double_against_spread,
         [HappeningType.PLAYING_DOUBLE_WITH]: double_with_spread,
     };
-    const resting_probability = stage.n_resting_players_per_rotation / stage.n_players;
-    const single_probability = stage.n_single_players / stage.n_players;
-    const double_probability = stage.n_double_players / stage.n_players;
-    const single_against_probability = Math.pow(single_probability, 2) / NChooseK(stage.n_single_players - 1, 1);
-    const double_with_probability = Math.pow(double_probability, 2) / NChooseK(stage.n_double_players - 1, 1);
     const min_cooldowns = {
         [HappeningType.RESTING]: CooldownForProbability(resting_probability),
-        [HappeningType.PLAYING_SINGLE]: CooldownForProbability(single_probability),
+        [HappeningType.PLAYING_SINGLE]: single_cooldown,
         [HappeningType.PLAYING_SINGLE_AGAINST]: CooldownForProbability(single_against_probability),
-        [HappeningType.PLAYING_DOUBLE]: CooldownForProbability(double_probability),
+        [HappeningType.PLAYING_DOUBLE]: double_cooldown,
         [HappeningType.PLAYING_DOUBLE_AGAINST]: double_against_cooldown,
         [HappeningType.PLAYING_DOUBLE_WITH]: CooldownForProbability(double_with_probability),
     };
